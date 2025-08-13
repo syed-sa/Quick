ab25fc1996ce460650f034838e96ba9bffecc627
syed <k.syed200367@gmail.com>
2025-08-13 07:37:42 +0530

    commit for adding images

--- BE/src/main/java/com/justsearch/backend/repository/ServicesRepository.java
+++ BE/src/main/java/com/justsearch/backend/repository/ServicesRepository.java
@@ -3,6 +3,8 @@ package com.justsearch.backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.justsearch.backend.model.Services;
@@ -13,8 +15,9 @@ public interface ServicesRepository extends JpaRepository<Services, Long> {
     // Checks if a service exists by service provider (user) ID and company name
     boolean existsByServiceProviderIdAndCompanyName(long serviceProviderId, String companyName);
 
-    // Find services by business category ID and postal code
-    List<Services> findByBusinessCategoryIdAndPostalCode(long businessCategoryId, String postalCode);
+   @Query("SELECT s FROM Services s WHERE s.businessCategory.id = :categoryId AND s.postalCode = :postalCode")
+List<Services> findByCategoryAndPostal(@Param("categoryId") Long categoryId, @Param("postalCode") String postalCode);
+
 
     // Find all services by service provider (user) ID
     List<Services> findAllByServiceProviderId(long serviceProviderId);
